8109627f517b9eb0825536bf0979cfd77bc01592
Tonipenyallop <kimtesu521@gmail.com>
2026-01-29 21:14:35 +0700

    add type

---
+++ src/types/github.ts
@@ -0,0 +1,22 @@
+export interface Repository {
+  id: number;
+  name: string;
+  full_name: string;
+  description: string | null;
+  forks_count: number;
+  language: string | null;
+  owner: {
+    login: string;
+    avatar_url: string;
+  } | null;
+}
+
+export interface OctokitResponse {
+  data: {
+    incomplete_results: boolean;
+    total_count: number;
+    items: Array<Repository>;
+  };
+}
+
+export type Sort = "desc" | "asc";
