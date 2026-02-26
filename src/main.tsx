1a34b11c2a25fb55b2d4ba30d141753dcb3c5b89
Tonipenyallop <kimtesu521@gmail.com>
2026-02-26 20:53:25 +0900

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Trend from "./components/Trend.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/trend" element={<Trend />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>,
+);
